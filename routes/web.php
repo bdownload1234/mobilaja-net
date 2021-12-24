d97f53a83724306f223f41c822a72b4ec8abbe94
bdownload1234 <bdownload1234@gmail.com>
2021-12-24 20:29:26 +0700

    Init Commit

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('welcomemobilaja');
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
