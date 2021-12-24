dda6100294c9942799d968e9182faffcab6bb0ad
bdownload1234 <bdownload1234@gmail.com>
2021-12-24 20:49:34 +0700

    env buat jimen

--- resources/views/welcomemobilaja.blade.php
+++ resources/views/welcomemobilaja.blade.php
@@ -99,7 +99,19 @@
 					   	</li>
 					   <li><a href='about_us.html'>About Us</a></li>
 					   <li><a href='contact_us.html'>Contact Us</a></li>
-					   <li><a href='login.html'><strong>Login</strong></a></li>
+					   {{-- <li><a href='login.html'><strong>Login</strong></a></li> --}}
+
+                        @if (Route::has('login'))
+                           @auth
+                               <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
+                           @else
+                               <li><a href="{{ route('login') }}"><strong>Login</strong></a></li>
+
+                               @if (Route::has('register'))
+                                   <li><a href="{{ route('register') }}">Register</a></li>
+                               @endif
+                           @endauth
+                        @endif
 					</ul>
 				</div>
 			</div>
